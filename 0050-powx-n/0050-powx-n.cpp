78675a0ac999e9f5b386b4151d13df0b3012a5d3
Nikunj Sharma <nikunjsharma14506@gmail.com>
2026-09-02 10:02:56 +0530

    Time: 0 ms (100%), Space: 8.6 MB (71.72%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long long power = n; 
+        
+        if (power < 0) {
+            x = 1.0 / x;
+            power = -power;
+        }
+        
+        double result = 1.0;
+        
+        while (power > 0) {
+            if (power & 1) result *= x;
+            x *= x;
+            power >>= 1;
+        }
+        
+        return result;
+    }
+};
